ab9c2312de12357420724238f91e46a9006c195f
ahsan870 <ahsanpabel@gmail.com>
2023-12-22 22:53:26 -0600

    Added birthday card updated!!

--- birthdayCard.py
+++ birthdayCard.py
@@ -1,16 +1,25 @@
 import streamlit as st
+import time
 
 def birthday_card():
     st.title("Happy Birthday!")
+    # Style the text
+    st.markdown(
+        "<h2 style='text-align: center; color: #FF69B4;'>Wishing you a fantastic birthday filled with joy and laughter!</h2>",
+        unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; font-size: 20px;'>🎉🎂🎁</p>", unsafe_allow_html=True)
+    # Button to toggle the birthday card
+    if st.button("Toggle Birthday Card"):
+        st.session_state.show_card = not st.session_state.get("show_card", False)
 
-    # Button to reveal the birthday card
-    if st.button("Open Birthday Card"):
+        # Add some animation when showing the card
+        if st.session_state.show_card:
+            st.balloons()
+
+    # Display the birthday card if the button is clicked
+    if st.session_state.get("show_card", False):
         # Replace "IMAGE_URL" with the actual URL of the image you want to use
-        image_url = "birthdatcard.jpg"
+        image_url = "images/black-birthday-with-balloonsCard.png"
         st.image(image_url, caption="Celebration Time", use_column_width=True)
-        st.write("Wishing you a fantastic birthday filled with joy and laughter!")
-        st.write("🎉🎂🎁")
-
 if __name__ == "__main__":
     birthday_card()
-
